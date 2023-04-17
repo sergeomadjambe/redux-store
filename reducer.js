998d393f1334707b05b04f0a5c1263badc971d06
Edna Omadjambe <ednaomadjambe@Ednas-Air.attlocal.net>
2023-04-17 08:34:48 -0400

    create redux

---
+++ reducer.js
@@ -0,0 +1,11 @@
+// reducers/index.js
+import { combineReducers } from 'redux';
+import productsReducer from './productsReducer';
+import cartReducer from './cartReducer';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+});
+
+export default rootReducer;
